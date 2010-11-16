cdbebce551a55144ec7b81cd45a0cad7c4cf038d
Alex Morega <alex@grep.ro>
2010-11-17 01:07:12 +0200

    factor in distance and defence growth

--- strategy.py
+++ strategy.py
@@ -1,6 +1,7 @@
 import math
 
-SWEET_GROWTH_RATE = 5
+DEFENCE_FACTOR = 1
+DISTANCE_FACTOR = 4
 ATTACK_FACTOR = 1.1
 
 def DoTurn(log, pw):
@@ -18,23 +19,29 @@ def DoTurn(log, pw):
     def distance(planet1, planet2):
         return pw.Distance(planet1.PlanetID(), planet2.PlanetID())
 
-    def sweet(planet):
-        return (SWEET_GROWTH_RATE * planet.GrowthRate() - planet.NumShips())
-
-    for target in sorted((p for p in pw.NotMyPlanets()),
-                         key=sweet, reverse=True):
-        friendly, enemy = in_flight(target)
-        if friendly > target.NumShips():
-            continue
-        else:
-            break # found a target
-    else:
-        return # no target!
-
-    max_ships = math.ceil(target.NumShips() * ATTACK_FACTOR)
     for source in pw.MyPlanets():
-        if max_ships < 1:
-            break
-        howmany = min( (max_ships, source.NumShips()) )
-        pw.IssueOrder(source.PlanetID(), target.PlanetID(), howmany)
-        max_ships -= howmany
+        def sweet(target):
+            f_growth = 10 * target.GrowthRate()
+            f_distance = DISTANCE_FACTOR * distance(source, target)
+            defence = target.NumShips()
+            if target.Owner() == 2:
+                defence += target.GrowthRate() * distance(source, target)
+            f_defence = DEFENCE_FACTOR * defence
+            return f_growth - f_defence - f_distance
+
+        ships_left = source.NumShips()
+
+        for target in sorted((p for p in pw.NotMyPlanets()),
+                             key=sweet, reverse=True):
+            friendly, enemy = in_flight(target)
+            defence_force = target.NumShips() - friendly
+            if target.Owner() == 2:
+                defence_force += target.GrowthRate() * distance(source, target)
+            if defence_force < 0:
+                continue
+
+            attack_force = math.ceil(defence_force * ATTACK_FACTOR)
+            if ships_left > attack_force:
+                pw.IssueOrder(source.PlanetID(), target.PlanetID(),
+                              attack_force)
+                ships_left -= attack_force
